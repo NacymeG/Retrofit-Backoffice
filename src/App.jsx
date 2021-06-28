cfedd6e162455f15dbe4ad55259f21df926b78a5
Loic <loic.brassart@gmail.com>
2021-06-28 16:45:33 +0200

    Basic admin app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import SampleComponent from './components/SampleComponent';
+import { Admin, Resource } from 'react-admin';
+import dataProvider from './dataProvider';
+import authProvider from './authProvider';
+import CharactersList from './components/Characters/List';
+import CharactersEdit from './components/Characters/Edit';
+import CharactersPost from './components/Characters/Post';
 
 function App() {
   return (
     <div className="App">
-      <SampleComponent />
+      <Admin authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource
+          name="characters"
+          list={CharactersList}
+          edit={CharactersEdit}
+          create={CharactersPost}
+        />
+      </Admin>
     </div>
   );
 }
